96353631561a3bd34f4d9c54b59dd01e055cd5eb
the-gigi <the.gigi@gmail.com>
2020-07-06 00:30:15 -0700

    remove dominator

--- dominator/kube_client.py
+++
@@ -1,4 +0,0 @@
-from kubernetes import client, config
-
-config.load_kube_config()
-kube_client = client.CustomObjectsApi()
--- dominator/player.py
+++
@@ -1,5 +0,0 @@
-# import dominion.object_model
-#
-# class Player()
-#     def __init__(self, player_type, game_client):
-#         self.game_client =
