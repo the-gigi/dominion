2b8a5ded00b39e6890169465037c7d23fcd340de
the-gigi <the.gigi@gmail.com>
2020-07-08 20:10:31 -0700

    Napoleon plays milita and witch

--- computer_players/napoleon.py
+++ computer_players/napoleon.py
@@ -8,34 +8,35 @@ class Napoleon(BaseComputerPlayer):
     def play_action_cards(self, hand):
         for i in range(self.state.actions):
             for card in hand:
-                if isinstance(card, Militia):
+                if isinstance(card, Witch):
                     self.play_card(card, hand)
-                elif isinstance(card, Spy):
+                elif isinstance(card, Militia):
                     self.play_card(card, hand)
 
     def buy(self, supply, money, buys):
-        if money == 4:
-            number = random.randint(0, 2)
-            card = 'Militia' if number == 1 else 'Spy'
-            return self.buy_card(money, card)
+        if money == 5:
+            return self.buy_card(money, 'Witch')
 
-        return False
+        if money == 4:
+            return self.buy_card(money, 'Militia')
+
+        return None
 
     def play_card(self, card, hand):
         self.game_client.play_action_card(card.Name())
         hand.remove(card)
 
-    def respond(self, request, *args):
-        if request == 'Spy':
-            top_card_names = args
-            response = {}
-            for name in top_card_names:
-                card = get_card_class(name)
-                response[name] = 'put_back'
-                if name == self.name:
-                    if card.Type in ('Victory', 'Curse'):
-                        response[name] = 'discard'
-                else:
-                    if card.Type not in ('Victory', 'Curse'):
-                        response[name] = 'discard'
-            return response
+    # def respond(self, request, *args):
+    #     if request == 'Spy':
+    #         top_card_names = args
+    #         response = {}
+    #         for name in top_card_names:
+    #             card = get_card_class(name)
+    #             response[name] = 'put_back'
+    #             if name == self.name:
+    #                 if card.Type in ('Victory', 'Curse'):
+    #                     response[name] = 'discard'
+    #             else:
+    #                 if card.Type not in ('Victory', 'Curse'):
+    #                     response[name] = 'discard'
+    #         return response
