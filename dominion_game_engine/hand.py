711cc24f4cae5a12e1f32756bae2f0ae142c774c
the-gigi <the.gigi@gmail.com>
2020-12-06 12:51:28 -0800

    add remove_by_name() helper function for hand (needed for Cellar)

--- dominion_game_engine/hand.py
+++ dominion_game_engine/hand.py
@@ -10,24 +10,28 @@ def has_card_type(hand, card_type: str):
     return any(c.Type == card_type for c in hand)
 
 
-def has_card_types(hand, card_types: List[str]):
+def has_card_types(hand, card_types: List[str]) -> bool:
     hand_counter = Counter(c.Type for c in hand)
     req_counter = Counter(card_types)
     for card_type, count in req_counter.items():
-        if count < hand_counter[card_type]:
+        if count < hand_counter[card_type] or hand_counter[card_type] == 0:
             return False
 
     return True
 
 
-def select_by_name(hand, card_names: List[str]):
+def select_by_name(hand, card_names: List[str]) -> List[BaseCard]:
     selected = []
     sorted_hand = sorted(hand, key=lambda c: c.Name())
     sorted_card_names = sorted(card_names)
     i = 0
     for card_name in sorted_card_names:
-        while sorted_hand[i].Name() < card_name:
+        while i < len(sorted_hand) and sorted_hand[i].Name() < card_name :
             i += 1
+
+        # missing card name
+        if i == len(sorted_hand):
+            return []
 
         card = sorted_hand[i]
         if card.Name() == card_name:
@@ -37,3 +41,11 @@ def select_by_name(hand, card_names: List[str]):
             raise RuntimeError(f'card name {card_name} is missing')
 
     return selected
+
+
+def remove_by_name(hand, card_names: List[str]):
+    for card_name in card_names:
+        for card in hand:
+            if card.Name() == card_name:
+                hand.remove(card)
+                break
