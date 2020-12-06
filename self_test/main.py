de6217cde96192509799d1d89957b8a089c57ed9
the-gigi <the.gigi@gmail.com>
2020-12-06 15:58:23 -0800

    added StatAtHomeSon player + Cellar card

--- self_test/main.py
+++ self_test/main.py
@@ -1,3 +1,4 @@
+from computer_players.stay_at_home_son import StayAtHomeSon
 from dominion_game_engine.game_factory import start_game
 from dominion_game_engine.cards import *
 from computer_players.rockefeller import Rockefeller
@@ -5,10 +6,27 @@ from computer_players.victor import Victor
 from computer_players.the_guy import TheGuy
 from computer_players.napoleon import Napoleon
 
+# card_types = [
+#     #Adventurer,
+#     Bureaucrat,
+#     #Chancellor,
+#     CouncilRoom,
+#     Festival,
+#     Library,
+#     Market,
+#     Militia,
+#     Moat,
+#     Smithy,
+#     #Spy,
+#     #Thief,
+#     Village,
+#     Witch]
+
 card_types = [
     #Adventurer,
-    Bureaucrat,
+    #Bureaucrat,
     #Chancellor,
+    Cellar,
     CouncilRoom,
     Festival,
     Library,
@@ -21,10 +39,12 @@ card_types = [
     Village,
     Witch]
 
+
 players_info = dict(Vicky=(Victor, None),
                     The_Rock=(Rockefeller, None),
                     Guy=(TheGuy, None),
-                    Dynamite=(Napoleon, None))
+                    Dynamite=(Napoleon, None),
+                    Zak_Galafinakis=(StayAtHomeSon, None))
 
 
 def main():
