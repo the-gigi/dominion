ed4c06ff8b5664df409b14d01e340e07e9b16ef6
the-gigi <the.gigi@gmail.com>
2021-02-20 02:37:19 -0800

    fixed hand card click handling when after disabling cards

--- grpc_networking/server/config.py
+++ grpc_networking/server/config.py
@@ -1,30 +1,9 @@
-from dominion_game_engine.cards import *
 from computer_players.rockefeller import Rockefeller
 from computer_players.victor import Victor
 from computer_players.the_guy import TheGuy
 from computer_players.napoleon import Napoleon
 from computer_players.stay_at_home_son import StayAtHomeSon
 from computer_players.robin_hood import RobinHood
-
-card_types = [
-    Artisan,
-    Bandit,
-    Bureaucrat,
-    # CouncilRoom,
-    Festival,
-    Library,
-    # Market,
-    Militia,
-    # Mine,
-    # Moat,
-    Remodel,
-    Sentry,
-    Smithy,
-    ThroneRoom,
-    # Village,
-    Workshop,
-    # Vassal,
-]
 
 computer_players = [
     ('Archy', RobinHood),
