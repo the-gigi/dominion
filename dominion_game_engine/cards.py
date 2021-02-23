51543f029feb988b91470de31bf04b55d37eed82
the-gigi <the.gigi@gmail.com>
2021-02-22 21:59:12 -0800

    comment out bureaucrat because UI has no responder

--- dominion_game_engine/cards.py
+++ dominion_game_engine/cards.py
@@ -117,15 +117,15 @@ class Bandit(BaseCard):
     Cost = 5
 
 
-class Bureaucrat(BaseCard):
-    Type = 'Action'
-    Text = """
-    Gain a silver card; put it on top of your deck.
-    Each other player reveals a Victory card from
-    his hand and puts it on top of his deck (or reveals
-    a hand with no Victory cards).
-    """
-    Cost = 4
+# class Bureaucrat(BaseCard):
+#     Type = 'Action'
+#     Text = """
+#     Gain a silver card; put it on top of your deck.
+#     Each other player reveals a Victory card from
+#     his hand and puts it on top of his deck (or reveals
+#     a hand with no Victory cards).
+#     """
+#     Cost = 4
 
 
 # class Chancellor(BaseCard):
